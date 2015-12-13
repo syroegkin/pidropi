e54b0fabc6dbd86bfabac5afd15b57039a249d77
Sergii Skorokhodov <sergii.skorokhodov@deliveryhero.com>
2015-12-13 19:32:40 +0100

    Backups, create subfolders structure

--- backup/mysql.py
+++ backup/mysql.py
@@ -1,6 +1,10 @@
 from subprocess import call
+from backup.backup import Backup
+import time
+import os
 
-class Mysql:
+
+class Mysql(Backup):
     """
     Mysql dump and backup
     """
@@ -11,21 +15,41 @@ class Mysql:
     mysql_passwd = None
     mysql_host = None
 
+    tmpPostfix = 'sql'
+
     def __init__(self, config):
         """Constructor
         :param config:
         """
+
+        super().__init__(config)
+
         # Checking first is everything is ok with config
         if 'databases' not in config or 'mysql' not in config['databases']:
             raise ValueError('No mysql, nothing to work with')
         if 'mysql' not in config:
             raise ValueError('Mysql connect details are necessary')
 
+        if 'tmp' not in config or 'tmpFolder' not in config['tmp']:
+            raise ValueError('Tmp folder is required for this job')
+
         # Getting databases list information
         self.set_databases(config['databases']['mysql'])
 
         # Getting mysql connection information
         self.set_mysql_credentials(config['mysql'])
+
+        # Seting tmp folder
+        self.set_tmp_folder(config['tmp']['tmpFolder'])
+
+        # Clean up old folders
+        self.cleanup_sub_folders()
+
+    def backup(self):
+        """
+        Make backup
+        """
+        self.dump_databases()
 
     def set_databases(self, databases):
         """Set up databases list to backup
@@ -45,9 +69,23 @@ class Mysql:
         self.mysql_host = cred['host']
         self.mysql_passwd = cred['passwd']
 
-    def backup(self):
+    def set_tmp_folder(self, folder):
+        """
+        Set up tmp folder, plus assigning sql postfix
+        :param folder: Tmp folder
+        """
+        self.tmpFolder = folder + '/' + self.tmpPostfix
+        super(Mysql, self).set_tmp_folder(self.tmpFolder)
+
+    def dump_databases(self):
         """Backup with mysqldump"""
-        call(["ls", "-l"])
-
-
-
+        folder_name = time.strftime("%Y%M%d%H%M%S")
+        os.mkdir(self.tmpFolder + '/' + folder_name)
+        for database in self.databases:
+            call(['mysqldump',
+                  '-u' + self.mysql_login,
+                  '-p' + self.mysql_passwd,
+                  '-h' + self.mysql_host,
+                  '-r' + self.tmpFolder + '/' + folder_name + '/' + database + '.sql',
+                  "%s" % database
+                  ])
