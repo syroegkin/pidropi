2433fc84904e0a140bb5903104e11569a89ca3ed
Sergii Skorokhodov <sergii.skorokhodov@deliveryhero.com>
2015-12-22 14:57:38 +0100

    Refactoring

--- backup/folder.py
+++ backup/folder.py
@@ -8,16 +8,16 @@ class Folder(Backup):
     tmpPostfix = 'folders'
 
     def __init__(self, config):
-
         super(Folder, self).__init__(config)
-
         if 'folders' not in config or 'folders' not in config['folders']:
             raise ValueError('No folders in config, nothing to work with')
-
-        # Set folders list
         self.set_folders(config['folders']['folders'])
 
     def set_folders(self, folders):
+        """
+        Set folders list
+        :param folders: folders string separated by comma
+        """
         self.folders = [x.strip() for x in folders.split(',')]
         if len(self.folders) == 0:
             raise ValueError('No folders in config')
@@ -27,16 +27,10 @@ class Folder(Backup):
         Make backup and clean up
         """
         self.dump_folders()
-
-        # Archive
-        # super(Folder, self).archive()
         self.archive()
-
-        # Clean up old folders
         self.cleanup_sub_folders()
 
     def dump_folders(self):
-        # folder_name = super(Folder, self).create_current_folder_by_time()
         folder_name = self.create_current_folder_by_time()
         for folder in self.folders:
             call(['rsync', '-ar',
--- tests/test_folder.py
+++ tests/test_folder.py
@@ -14,26 +14,43 @@ class TestFolder(unittest.TestCase):
         with self.assertRaises(ValueError):
             folder = Folder(config_changed)
 
-    def test_init_single_folder(self):
+    @mock.patch.object(Folder, 'set_folders')
+    def test_init_single_folder(self, mock_set_folders):
         folder = Folder(config)
-        self.assertEqual(folder.folders, config['folders']['folders'].split(','))
+        self.assertTrue(mock_set_folders.called)
+
+    def test_set_folders_single_folder(self):
+        folder = Folder(config)
+        folders = '/tmp/pmt'
+        folder.set_folders(folders)
+        self.assertEqual(folder.folders, [folders])
 
     def test_set_folders_multiple_folders(self):
-        config_changed = copy.deepcopy(config)
-        config_changed['folders']['folders'] = '/tmp/1,/tmp/2'
-        folder = Folder(config_changed)
+        folder = Folder(config)
+        two_folders = '/tmp/1,/tmp/2'
+        folder.set_folders(two_folders)
         self.assertEqual(folder.folders,
-                         [x.strip() for x in config_changed['folders']['folders'].split(',')])
+                         [x.strip() for x in two_folders.split(',')])
 
-        config_changed['folders']['folders'] = '/tmp/1, /tmp/2,/tmp/3,    /tmp/4'
-        folder = Folder(config_changed)
+        more_folders = '/tmp/1, /tmp/2,/tmp/3,    /tmp/4'
+        folder.set_folders(more_folders)
         self.assertEqual(folder.folders,
-                         [x.strip() for x in config_changed['folders']['folders'].split(',')])
+                         [x.strip() for x in more_folders.split(',')])
 
     @mock.patch('backup.folder.call')
-    @mock.patch('backup.backup.os')
-    def test_dump_folders(self, mock_os, mock_call):
+    @mock.patch.object(Folder, 'create_current_folder_by_time')
+    def test_dump_folders(self, mock_create, mock_call):
         folder = Folder(config)
         folder.dump_folders()
-        self.assertTrue(mock_os.mkdir.called)
+        self.assertTrue(mock_create.called)
         self.assertTrue(mock_call.called)
+
+    @mock.patch.object(Folder, 'dump_folders')
+    @mock.patch.object(Folder, 'archive')
+    @mock.patch.object(Folder, 'cleanup_sub_folders')
+    def test_backup(self, mock_cleanup, mock_archive, mock_dump):
+        folder = Folder(config)
+        folder.backup()
+        self.assertTrue(mock_cleanup.called)
+        self.assertTrue(mock_archive.called)
+        self.assertTrue(mock_dump.called)
